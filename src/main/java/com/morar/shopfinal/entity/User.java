c085ce535eea09423f4117776fc1c62f894b2b97
Gena <genamorar3@gmail.com>
2021-11-25 20:12:15 +0200

    Have added user entity and user repository

---
+++ src/main/java/com/morar/shopfinal/entity/User.java
@@ -0,0 +1,63 @@
+package com.morar.shopfinal.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    @Column(name = "mail",nullable = false)
+    private  String mail;
+
+    @Column(name = "phone",nullable = false)
+    private String phone;
+
+    @Column(name = "role",nullable = false)
+    private String role;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
