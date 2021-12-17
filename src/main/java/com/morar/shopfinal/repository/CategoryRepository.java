dad6f6db60aa820dd2b8856a50f3a35c4fad5922
Gena <genamorar3@gmail.com>
2021-12-17 13:42:36 +0200

    Category functionality has been added

--- src/main/java/com/morar/shopfinal/repository/CategoryRepository.java
+++ src/main/java/com/morar/shopfinal/repository/CategoryRepository.java
@@ -2,6 +2,16 @@ package com.morar.shopfinal.repository;
 
 import com.morar.shopfinal.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface CategoryRepository extends JpaRepository <Category, Long> {
+
+    Category findByName(String name);
+
+    boolean existsCategoriesByName(String name);
+
+    long deleteByName(String name);
+
 }
