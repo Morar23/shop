98c10c4d7b56d2072fdbb2a2811a226b2fe1d110
ivan.stoianov <ivan.stoianov@sap.com>
2022-01-26 17:44:09 +0200

    refactoring

--- src/main/java/com/morar/shopfinal/dao/impl/AnnouncementDAOImpl.java
+++ src/main/java/com/morar/shopfinal/dao/impl/AnnouncementDAOImpl.java
@@ -10,7 +10,6 @@ import com.morar.shopfinal.repository.AnnouncementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
--- src/main/java/com/morar/shopfinal/dao/impl/UserDAOImpl.java
+++ src/main/java/com/morar/shopfinal/dao/impl/UserDAOImpl.java
@@ -6,10 +6,8 @@ import com.morar.shopfinal.entity.User;
 import com.morar.shopfinal.exception.UserNotFoundException;
 import com.morar.shopfinal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
