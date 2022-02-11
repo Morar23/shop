2cb9ff440893b2dba9f2701eb61e856a45700352
ivan.stoianov <ivan.stoianov@sap.com>
2022-02-11 21:17:38 +0200

    refactoring

--- src/main/java/com/morar/shopfinal/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/morar/shopfinal/controller/GlobalExceptionHandlerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalControllerExceptionHandler {
+public class GlobalExceptionHandlerController {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleExceptions(Throwable e) {
         if (e instanceof EntityNotFoundException) {
